fa1adf6c147e6b6fb5fa41479eb3983205c7e69c
Антонюк Евгений Викторович <96491514+evgenijantonyuk@users.noreply.github.com>
2026-08-23 13:54:46 +0700

    task 7

--- js/hw-1.js
+++ js/hw-1.js
@@ -15,3 +15,37 @@ const jsCreated = Number(1995)
 alert(`В ${jsCreated} году ${jsAuthor} создал язык програмирования JavaScript.`)
 
 // 4
+let var1 = 10
+let var2 = 2
+
+let sum = var1 + var2
+let difference = var1 - var2
+let multiplication = var1 * var2
+let division = var1 / var2
+
+alert (sum)
+alert (difference)
+alert (multiplication)
+alert (division)
+
+// 5
+let two = 2
+let result = two ** 5
+alert(result)
+
+// 6
+let aA = 9
+let b = 2
+alert(aA % b)
+
+// 7
+let num = 1
+num += 5
+num -= 3
+num *= 7
+num /= 3
+num++
+num--
+alert(num)
+
+// 8
