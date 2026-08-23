af2dda0115101620e2614698782303900572cdbc
Антонюк Евгений Викторович <96491514+evgenijantonyuk@users.noreply.github.com>
2026-08-23 11:19:24 +0700

    task 3

--- js/hw-1.js
+++ js/hw-1.js
@@ -1,6 +1,17 @@
 
-
+// 1
 let a = 10
 alert(a)
 a = 20
 alert(a)
+
+// 2
+const fistIponeYear = Number(2007)
+alert(`Первая модель смартфона iPhone выпущена в ${fistIponeYear} году`)
+
+// 3
+const jsAuthor = String('Brendan Eich')
+const jsCreated = Number(1995)
+alert(`В ${jsCreated} году ${jsAuthor} создал язык програмирования JavaScript.`)
+
+// 4
