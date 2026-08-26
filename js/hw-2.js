ef93a40862d04737c3cb655198de75912a6df687
Антонюк Евгений Викторович <96491514+evgenijantonyuk@users.noreply.github.com>
2026-08-27 00:34:52 +0700

    test 1

---
+++ js/hw-2.js
@@ -0,0 +1,11 @@
+// task 1
+let password = 'pass'
+let userPassword = String(prompt('Введите пароль:'))
+
+if (password === userPassword) {
+      console.log('Пароль введен верно')
+} else {
+      console.log('Пароль введен неправильно')
+}
+
+// extra task
