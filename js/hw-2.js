fd4f797ea73112acb03305cbca5d521614b7d862
Антонюк Евгений Викторович <96491514+evgenijantonyuk@users.noreply.github.com>
2026-08-27 15:47:36 +0700

    task 5 fix

--- js/hw-2.js
+++ js/hw-2.js
@@ -46,25 +46,25 @@ if (isNaN(monthNumber) || monthNumber < 1 || monthNumber > 12) {
             case 1:
             case 2:
                   season = 'Зима'
-                  console.log('Зима');
+                  console.log(season);
                   break;
             case 3:
             case 4:
             case 5:
                   season = 'Весна'
-                  console.log('Весна');
+                  console.log(season);
                   break;
             case 6:
             case 7:
             case 8:
                   season = 'Лето'
-                  console.log('Лето');
+                  console.log(season);
                   break;
             case 9:
             case 10:
             case 11:
                   season = 'Осень'
-                  console.log('Осень');
+                  console.log(season);
                   break;
             default:
                  season = "неизвестно";
