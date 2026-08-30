6bd279a780a2297e3b146e5730c3829fab96ce8d
Антонюк Евгений Викторович <96491514+evgenijantonyuk@users.noreply.github.com>
2026-08-30 17:39:53 +0700

    task 1-3

--- js/hw-3.js
+++ js/hw-3.js
@@ -1,12 +1,23 @@
+// task 1
 let i = 0
-while (i <= 5) {
-      console.log(i)
-      i++
-}
 
-let a = 0
 do {
-      console.log(a)
-      i++
-      
-} while (a <= 5)
+  console.log("Привет")
+  i++
+} while (i < 2)
+
+// task 2
+let a = 0
+
+do {
+      a++
+    console.log(a)  
+} while (a < 5)
+
+// task 3 
+let b = 6
+
+do {
+      b++
+    console.log(b)  
+} while (b < 22)
